f10e3da17ef5325ccd346f081a4b0ff0f23b1fad
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2018-08-12 11:44:51 -0400

    added mongodb

--- siem/config/datastores.js
+++ siem/config/datastores.js
@@ -48,8 +48,8 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
+    adapter: 'sails-mongo',
+    url: 'mongodb://siem:siem@127.0.0.1:27017/siem',
 
   },
 
