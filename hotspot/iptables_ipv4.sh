c8b6790f085a5418eb1209ecbf462c850bc49958
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2018-08-19 12:37:55 -0400

    added hotspot

---
+++ hotspot/iptables_ipv4.sh
@@ -0,0 +1,3 @@
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo iptables -A FORWARD -i eth0 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i wlan1 -o eth0 -j ACCEPT
