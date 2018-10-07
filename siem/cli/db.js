dc6574f0e9b78b09aef59c7c854b40f8da475199
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2018-10-07 09:10:16 +0000

    updated codebase

--- siem/cli/db.js
+++ siem/cli/db.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient
-const url = "mongodb://siem:siem@localhost:27017/siem"
+const url = "mongodb://@localhost:27017/siem"
 
 // exporting default db client and url
 module.exports = {
